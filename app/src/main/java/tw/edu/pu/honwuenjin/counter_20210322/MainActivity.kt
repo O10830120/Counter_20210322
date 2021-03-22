df92c36a03fb524e1286b4c118dd58916038f6bd
honwuenjin <o1083012@gm.pu.edu.tw>
2021-03-22 15:25:58 +0800

    OnClickListener監聽界面

--- app/src/main/java/tw/edu/pu/honwuenjin/counter_20210322/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/honwuenjin/counter_20210322/MainActivity.kt
@@ -2,19 +2,29 @@ package tw.edu.pu.honwuenjin.counter_20210322
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,
+    View.OnClickListener {
 
     var counter:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        txv.setOnClickListener(this)
+        img.setOnClickListener(this)
+        btn4.setOnClickListener(this)
     }
 
     fun AddOne(v: View){
+        counter++
+        txv.text = counter.toString()
+    }
+
+    fun AddTwo(v: View){
         counter++
         txv.text = counter.toString()
     }
@@ -32,5 +42,23 @@ class MainActivity : AppCompatActivity() {
 
         txv.text = counter.toString()
     }
-}
+
+    override fun onClick(v: View?) {
+        if(v==txv){
+        counter++
+    }
+    else if (v==img) {
+            counter += 2
+        }
+        else {
+            counter=(1..100).random()
+            txv.text = counter.toString()
+        }
+        txv.text = counter.toString()
+    }
+
+
+    }
+
+
 
